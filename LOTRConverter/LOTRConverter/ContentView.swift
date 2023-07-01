f8c778f80709c2eecabd07aac39a93430f4a6f59
Praveen <rpraveen.engineer@gmail.com>
2023-07-01 15:33:04 +0530

    Added LOTRConverter project setup

---
+++ LOTRConverter/LOTRConverter/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  LOTRConverter
+//
+//  Created by Praveen R on 01/07/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LOTRConverter/LOTRConverter/LOTRConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LOTRConverterApp.swift
+//  LOTRConverter
+//
+//  Created by Praveen R on 01/07/23.
+//
+
+import SwiftUI
+
+@main
+struct LOTRConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
